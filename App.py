04434f96a2527ef4ddab0c5710197efed57f5265
sharan767 <127106387+sharan767@users.noreply.github.com>
2026-04-15 15:09:36 +0530

    Update App.py

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 @st.cache_resource
 def load_summarizer():
   return pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
-  summarizer = load_summarizer()
+summarizer = load_summarizer()
 #streamlit UI
 st.title("EVO AI Text Summarizer")
 st.write("Enter a long text below, and get a concise summary!")
